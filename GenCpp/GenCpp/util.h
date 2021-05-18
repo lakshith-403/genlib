f5ca31d5c7ba3a0058c88cd3a70f898d2fcc35e2
Lakshith Nishshanke <lakshith.k.nishshanke@gmail.com>
2021-05-18 06:53:54 +0530

    Make util Static

--- GenCpp/GenCpp/util.h
+++ GenCpp/GenCpp/util.h
@@ -6,10 +6,10 @@ class util
 {
 public:
 
-	int rand(int l, int r);
+	static int rand(int l, int r);
 
-	std::vector<std::vector<char>> generateCharGenes(int geneLength, int genes);
+	static std::vector<std::vector<char>> generateCharGenes(int geneLength, int genes);
 
-	std::vector<std::vector<int>> generateIntGenes(int geneLength, int genes, int l, int r);
+	static std::vector<std::vector<int>> generateIntGenes(int geneLength, int genes, int l, int r);
 };
 
